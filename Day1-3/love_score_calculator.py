21e4f204a97361ef25078f3257c5ec527a41216e
shivaharigaj <shivaharigaj@gmail.com>
2024-07-19 15:52:58 +0545

    ..

--- Day1-3/love_score_calculator.py
+++ Day1-3/love_score_calculator.py
@@ -1,7 +1,10 @@
-print("The Love Calculator is Calculating your score . . . . . ")
+print("The Love Calculator is Calculating your score . . . . . \n\n")
 
 name1 = input("Enter Your name:- ")
+print("\n")
 name2 = input("Input your Lover name:- ")
+print("\n")
+
 
 combined_names = name1+name2
 lower_names = combined_names.lower()
@@ -18,10 +21,17 @@ v = lower_names.count("v")
 e = lower_names.count("e")
 second_digit = l+o+v+e
 
-score = int(str(first_digit)+str(second_digit))
+if combined_names :
+    score = 100
+else:
+    score = int(str(first_digit)+str(second_digit))
+
 if score<10 or score>90:
     print(f"Your score is {score}, You together like coke and mentos. ")
+    print("\n\n")
 elif score>=40 and score<=50:
     print(f"Your Score is {score}, You alright together. ")
+    print("\n\n")
 else:
     print(f"Your love score is {score}")
+    print("\n\n")
